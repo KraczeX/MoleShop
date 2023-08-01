d0d840e670df2f9f65a9f07117454c1bf3dfdbc8
Krecik <bartoszkraczek2006@gmail.com>
2023-08-01 19:36:16 +0200

    update

--- public/projects.js
+++ public/projects.js
@@ -1,5 +1,5 @@
 const projects = [
-    {id: 1, name: 'CodingMole', description: 'Our website, built using React and Tailwind CSS, offers interactivity, responsiveness, and an aesthetically pleasing design, providing a unique user experience.', image: 'src/assets/CodingMole.png'},
+    {id: 1, name: 'CodingMole', description: 'Our website, built using React and Tailwind CSS, offers interactivity, responsiveness, and an aesthetically pleasing design, providing a unique user experience.', image: 'src/assets/CodingMole.jpeg'},
 
 ]
 
